1dbdd01eb58f15c5e79e104283dc6986bf6a0436
ADRITA CHAKRABORTY <chakrabortyadrita04@gmail.com>
2024-03-03 18:42:00 +0530

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/graphic.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "pattern image";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My ML Projects",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "ML projects by me.",
+    url: "https://github.com/ADRITA-art/ML-Projects",
   },
   {
-    title: "Web Development for Beginners",
+    title: "LinkedIn profile",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is my linkedIn profile. You can check my profile here.",
+    url: "https://www.linkedin.com/in/adrita-chakraborty-ba9b2a24b/",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Includes my experience and design abilities.",
+    url: "https://docs.google.com/document/d/16GH2QktQKQKOwv1EbUbx2MlC-2_ccIIuZBwH-3-JdkM/edit?usp=sharing",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "github link",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is my github profile. You can check my profile here.",
+    url: "https://github.com/ADRITA-art",
   },
 ];
 
